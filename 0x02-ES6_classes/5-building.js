a3f55570ad9fe8b5ef009e276fd27a71b053a0d1
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-10-01 20:37:05 +0100

    (feat) -> impement an abstract class, throw error if a method is not implemented in the inheriting class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+}

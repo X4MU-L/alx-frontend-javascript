8e75d265070b913b260724eef8c5aefdf40ef816
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-10-01 20:55:55 +0100

    (feat) -> implement a class, add getters, setters and methods

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-underscore-dangle */
+export default class Airport {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
+}

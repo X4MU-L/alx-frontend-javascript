8d9e33e0bd8f99b66037a555cfd99e031b65b431
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-10-01 20:16:59 +0100

    (feat) -> implement a class, add getters, setters and methods

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-underscore-dangle */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw TypeError('Currency must be instance of Currency');
+    }
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw TypeError('Rates or Amount must be a number');
+    }
+
+    return amount * conversionRate;
+  }
+}

d75f5c4fb4211b85b5279598c369b72d92446b4e
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-10-03 20:50:57 +0100

    (refactor) -> remove error thrown

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,17 @@
-import getStudentsByLocation from './2-get_students_by_loc';
-
 export default function updateStudentGradeByCity(students, city, grades) {
   const gradesObj = {};
   grades.forEach((grade) => {
     gradesObj[grade.studentId] = grade.grade;
   });
-  // eslint-disable-next-line arrow-body-style
-  return getStudentsByLocation(students, city).map((student) => {
-    return gradesObj[student.id] !== undefined
-      ? { ...student, grade: gradesObj[student.id] }
-      : { ...student, grade: 'N/A' };
-  });
+
+  return (
+    students
+      .filter((student) => student.location === city)
+      // eslint-disable-next-line arrow-body-style
+      .map((student) => {
+        return gradesObj[student.id] !== undefined
+          ? { ...student, grade: gradesObj[student.id] }
+          : { ...student, grade: 'N/A' };
+      })
+  );
 }

2e964a1d8790580f1b10c08aff1c0288d28a94b8
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-10-04 01:44:23 +0100

    (fix) -> check for Map instance

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,11 @@
 export default function updateUniqueItems(mapObject) {
-  let updated = false;
+  if (!(mapObject instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
   for (const [key, value] of mapObject) {
     if (value === 1) {
-      updated = true;
       mapObject.set(key, 100);
     }
-  }
-  if (!updated) {
-    throw new Error('Cannot process');
   }
 }

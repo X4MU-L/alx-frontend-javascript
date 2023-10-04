3c2b1e47569982aa1fa00cd346a5b353704b6477
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-10-04 02:45:04 +0100

    (fix) -> check if instance of a Set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,13 @@
 export default function cleanSet(setObject, startstring) {
-  const { length } = startstring;
+  if (startstring === undefined || startstring.length === 0) {
+    return '';
+  }
   const result = [];
 
   if (setObject instanceof Set) {
     for (const [item] of setObject.entries()) {
-      if (item.startsWith(startstring) && length) {
-        result.push(item.slice(length));
+      if (item.startsWith(startstring)) {
+        result.push(item.slice(startstring.length));
       }
     }
   }
